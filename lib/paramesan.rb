5096f9a15dc9a6bcad5508f47521704aa7bc2fc5
Jeff Pace <jpace@softwareag.com>
2017-05-03 14:12:05 -0400

    Add initial version of parameterized test support

---
+++ lib/paramesan.rb
@@ -0,0 +1,23 @@
+#!/usr/bin/ruby -w
+# -*- ruby -*-
+
+module Paramesan
+  def param_test paramlist, &blk
+    puts "self: #{self}"
+    
+    paramlist.each do |params|
+      basename = "test_" + params.to_s.gsub(Regexp.new('[^\w]+'), '_')
+      mname = basename
+
+      count = 1
+      while instance_methods.include? mname.to_sym
+        mname += "_#{count}"
+      end
+
+      define_method mname do
+        instance_exec(params, &blk)
+        puts
+      end
+    end
+  end
+end
